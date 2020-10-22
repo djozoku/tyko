e8d593cf86b22eda6c7f60da0f6a8f8d1fe6f34b
Ozoku <dragon.djozoku@gmail.com>
2020-10-22 22:15:00 +0300

    chore: linting config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off', // we need to use I prefixed interfaces
+    '@typescript-eslint/no-explicit-any': 'off', // we need to sometimes get off the type system
+    'import/no-unresolved': 'off', // typescript already knows what imports resolve
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
